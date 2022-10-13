f005905d9f45fdab05ad94474529bbd298d62706
sampiercy <sam.piercy@justpark.com>
2022-10-13 16:02:11 +0100

    fix sample app

--- sample/src/main/java/com/chuckerteam/chucker/sample/MainActivity.kt
+++ sample/src/main/java/com/chuckerteam/chucker/sample/MainActivity.kt
@@ -2,30 +2,17 @@ package com.chuckerteam.chucker.sample
 
 import android.os.Bundle
 import android.os.StrictMode
-import android.text.method.LinkMovementMethod
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.chuckerteam.chucker.api.Chucker
-import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.sample.databinding.ActivityMainSampleBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-
-private val interceptorTypeSelector = InterceptorTypeSelector()
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainSampleBinding
 
-    private val client by lazy {
-        createOkHttpClient(applicationContext, interceptorTypeSelector)
-    }
-
-    private val httpTasks by lazy {
-        listOf(HttpBinHttpTask(client), DummyImageHttpTask(client), PostmanEchoHttpTask(client))
+    private val client: HttpBinClient by lazy {
+        HttpBinClient(applicationContext)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,43 +22,14 @@ class MainActivity : AppCompatActivity() {
 
         with(mainBinding) {
             setContentView(root)
-            doHttp.setOnClickListener {
-                for (task in httpTasks) {
-                    task.run()
-                }
-            }
+            doHttp.setOnClickListener { client.doHttpActivity() }
+            triggerException.setOnClickListener { client.recordException() }
 
             launchChuckerDirectly.visibility = if (Chucker.isOp) View.VISIBLE else View.GONE
             launchChuckerDirectly.setOnClickListener { launchChuckerDirectly() }
-
-            exportToFile?.visibility = if (Chucker.isOp) View.VISIBLE else View.GONE
-            exportToFile?.setOnClickListener {
-                lifecycleScope.launch {
-                    val uri = withContext(Dispatchers.IO) {
-                        ChuckerCollector(this@MainActivity)
-                            .writeTransactions(this@MainActivity, null)
-                    }
-                    if (uri == null) {
-                        Toast.makeText(applicationContext, R.string.export_to_file_failure, Toast.LENGTH_SHORT).show()
-                    } else {
-                        val successMessage = applicationContext.getString(R.string.export_to_file_success, uri.path)
-                        Toast.makeText(applicationContext, successMessage, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-
-            interceptorTypeLabel.movementMethod = LinkMovementMethod.getInstance()
-            useApplicationInterceptor.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    interceptorTypeSelector.value = InterceptorType.APPLICATION
-                }
-            }
-            useNetworkInterceptor.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    interceptorTypeSelector.value = InterceptorType.NETWORK
-                }
-            }
         }
+
+        client.initializeCrashHandler()
 
         StrictMode.setVmPolicy(
             StrictMode.VmPolicy.Builder()
@@ -80,19 +38,10 @@ class MainActivity : AppCompatActivity() {
                 .penaltyDeath()
                 .build()
         )
-
-        StrictMode.setThreadPolicy(
-            StrictMode.ThreadPolicy.Builder()
-                .detectDiskReads()
-                .detectDiskWrites()
-                .penaltyLog()
-                .penaltyDeath()
-                .build()
-        )
     }
 
     private fun launchChuckerDirectly() {
         // Optionally launch Chucker directly from your own app UI
-        startActivity(Chucker.getLaunchIntent(this))
+        startActivity(Chucker.getLaunchIntent(this, Chucker.SCREEN_HTTP))
     }
 }
